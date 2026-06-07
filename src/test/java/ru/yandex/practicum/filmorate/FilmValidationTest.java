dde4d750ff24df364deb3277324aaa4d2ba9f975
Ivan Krasilnikiov <ikra-nn@yandex.ru>
2026-06-07 15:32:12 +0300

    Исправил замечания ревьюера

--- src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -20,6 +21,7 @@ class FilmValidationTest {
     }
 
     @Test
+    @DisplayName("Добавить валидный фильм")
     void shouldAddValidFilm() {
         Film film = makeFilm("Название", "Описание", LocalDate.of(2000, 1, 1), 120);
         Film result = controller.addFilm(film);
@@ -27,12 +29,14 @@ class FilmValidationTest {
     }
 
     @Test
+    @DisplayName("Ошибка при пустом названии")
     void shouldFailOnEmptyName() {
         Film film = makeFilm("", "Описание", LocalDate.of(2000, 1, 1), 120);
         assertThrows(ValidationException.class, () -> controller.addFilm(film));
     }
 
     @Test
+    @DisplayName("Ошибка при описании длиннее 200 символов")
     void shouldFailOnDescriptionOver200() {
         String longDesc = "а".repeat(201);
         Film film = makeFilm("Название", longDesc, LocalDate.of(2000, 1, 1), 120);
@@ -40,6 +44,7 @@ class FilmValidationTest {
     }
 
     @Test
+    @DisplayName("Описание ровно 200 символов — допустимо")
     void shouldPassOnDescriptionExactly200() {
         String desc = "а".repeat(200);
         Film film = makeFilm("Название", desc, LocalDate.of(2000, 1, 1), 120);
@@ -47,24 +52,28 @@ class FilmValidationTest {
     }
 
     @Test
+    @DisplayName("Ошибка при дате релиза раньше 28.12.1895")
     void shouldFailOnReleaseDateBefore1895() {
         Film film = makeFilm("Название", "Описание", LocalDate.of(1895, 12, 27), 120);
         assertThrows(ValidationException.class, () -> controller.addFilm(film));
     }
 
     @Test
+    @DisplayName("Дата релиза 28.12.1895 — допустима")
     void shouldPassOnReleaseDateExactly1895() {
         Film film = makeFilm("Название", "Описание", LocalDate.of(1895, 12, 28), 120);
         assertDoesNotThrow(() -> controller.addFilm(film));
     }
 
     @Test
+    @DisplayName("Ошибка при нулевой продолжительности")
     void shouldFailOnZeroDuration() {
         Film film = makeFilm("Название", "Описание", LocalDate.of(2000, 1, 1), 0);
         assertThrows(ValidationException.class, () -> controller.addFilm(film));
     }
 
     @Test
+    @DisplayName("Ошибка при отрицательной продолжительности")
     void shouldFailOnNegativeDuration() {
         Film film = makeFilm("Название", "Описание", LocalDate.of(2000, 1, 1), -1);
         assertThrows(ValidationException.class, () -> controller.addFilm(film));
--- src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -20,6 +21,7 @@ class UserValidationTest {
     }
 
     @Test
+    @DisplayName("Создать валидного пользователя")
     void shouldCreateValidUser() {
         User user = makeUser("test@mail.ru", "login", "Имя", LocalDate.of(1990, 1, 1));
         User result = controller.createUser(user);
@@ -28,6 +30,7 @@ class UserValidationTest {
     }
 
     @Test
+    @DisplayName("Имя заменяется логином если пустое")
     void shouldUseLoginAsNameWhenNameIsEmpty() {
         User user = makeUser("test@mail.ru", "login", "", LocalDate.of(1990, 1, 1));
         User result = controller.createUser(user);
@@ -35,36 +38,42 @@ class UserValidationTest {
     }
 
     @Test
+    @DisplayName("Ошибка при пустом email")
     void shouldFailOnEmptyEmail() {
         User user = makeUser("", "login", "Имя", LocalDate.of(1990, 1, 1));
         assertThrows(ValidationException.class, () -> controller.createUser(user));
     }
 
     @Test
+    @DisplayName("Ошибка при email без символа @")
     void shouldFailOnEmailWithoutAt() {
         User user = makeUser("wrongemail.ru", "login", "Имя", LocalDate.of(1990, 1, 1));
         assertThrows(ValidationException.class, () -> controller.createUser(user));
     }
 
     @Test
+    @DisplayName("Ошибка при пустом логине")
     void shouldFailOnEmptyLogin() {
         User user = makeUser("test@mail.ru", "", "Имя", LocalDate.of(1990, 1, 1));
         assertThrows(ValidationException.class, () -> controller.createUser(user));
     }
 
     @Test
+    @DisplayName("Ошибка при логине с пробелами")
     void shouldFailOnLoginWithSpaces() {
         User user = makeUser("test@mail.ru", "log in", "Имя", LocalDate.of(1990, 1, 1));
         assertThrows(ValidationException.class, () -> controller.createUser(user));
     }
 
     @Test
+    @DisplayName("Ошибка при дате рождения в будущем")
     void shouldFailOnFutureBirthday() {
         User user = makeUser("test@mail.ru", "login", "Имя", LocalDate.now().plusDays(1));
         assertThrows(ValidationException.class, () -> controller.createUser(user));
     }
 
     @Test
+    @DisplayName("Дата рождения сегодня — допустима")
     void shouldPassOnTodayBirthday() {
         User user = makeUser("test@mail.ru", "login", "Имя", LocalDate.now());
         assertDoesNotThrow(() -> controller.createUser(user));
