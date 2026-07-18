6bc2da9992bfe6d3518cffd68cf176b1ecc1fb69
Ivan Krasilnikiov <ikra-nn@yandex.ru>
2026-07-18 17:23:25 +0300

    Fix checkstyle: add empty lines between methods in FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -7,11 +7,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film add(Film film);
+
     Film update(Film film);
+
     Collection<Film> findAll();
+
     Optional<Film> findById(int id);
+
     void addLike(int filmId, int userId);
+
     void removeLike(int filmId, int userId);
+
     List<Film> getPopular(int count);
 }
